69cc05571dd0c3c2badabf545bddfdc4a278ca50
Eric093 <eric.blot1@gmail.com>
2024-09-19 09:56:23 +0200

    main.py OK

--- fasthtml-2/main.py
+++ fasthtml-2/main.py
@@ -1,20 +1,41 @@
 from fasthtml.common import *
 
-app,rt = fast_app(live=True)
+def render(todo):
+    tid = f'todo-{todo.id}'
+    toggle =A('Toggle', hx_get=f'/toggle/{todo.id}', target_id=tid)
+    return Li(toggle, 
+              Str(todo.title) + (" []"  if todo.done else ""),
+               id=tid)
+
+app,rt,todos,Todo = fast_app('todo.db', live=True, render=render,
+                              id=int, title=str, done=bool, pk='id')
+
+# def NumList(i):
+#     return Ul(*[Li(o) for o in range(i)])
+
+
+
 
 @rt('/')
 def get(): 
-    nums = Ul(*[Li(o) for o in range(10)])
-    return Titled('Salut !',
-                  nums,
-                  Div(P(H1('Hello World!')),
-                    P(A('LIEN', href='/change')))
-                    )
+    # todos.insert(Todo(title='Un Todo', done=False))
+    # items = [Li(o) for o in todos()]
+    return Titled('Todos',
+                  # Div(nums, id='stuff', hx_get="/change"),
+                  Ul(*todos()),
+                                      )
 
-@rt('/change')
-def get(): 
-    return Div(P('Nice to be here!'),
-                    P(A('Home', href='/')))
+@rt('/toggle/{tid}')
+def get(tid:int):
+    todo = todos[tid]
+    todo.done = not todo.done
+    todos.update(todo)
+    return todo
+
+# @rt('/change')
+# def get(): 
+#     return P('Nice to be here!')
+                    
 
 
 serve()
