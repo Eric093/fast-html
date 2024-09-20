50fdbbb2d6c44e93126c4243e5502c4a2bce8a66
Eric093 <eric.blot1@gmail.com>
2024-09-20 11:56:25 +0200

    Début de session

--- demo/main.py
+++ demo/main.py
@@ -1,6 +1,33 @@
+
+"""
+This module provides a simple web application for managing a todo list using the FastHTML framework.
+
+Functions:
+    render(item: 'Todo') -> Li:
+        Renders a todo item as an HTML list item with a delete link.
+
+    get(request) -> Titled:
+        Handles GET requests to the root URL ("/"). Returns an HTML page with a form to add new todo items,
+        a list of existing todo items, and a logout link.
+
+    post(todo: 'Todo') -> None:
+        Handles POST requests to the root URL ("/"). Inserts a new todo item into the database.
+
+    delete(id: int) -> None:
+        Handles DELETE requests to the URL pattern "/todo/{id}". Deletes the specified todo item from the database.
+
+Variables:
+    auth:
+        Middleware for user authentication using username and password.
+
+    app, rt, todos, Todo:
+        FastHTML application instance, route decorator, todo items collection, and Todo model class.
+
+    serve:
+        Function to start the FastHTML application server.
+"""
+
 from fasthtml.common import *
-
-
 
 def render(item:'Todo'):
     id = f'todo-{item.id}'
@@ -14,6 +41,7 @@ app,rt,todos,Todo = fast_app(
     id=int, title=str, pk='id')
 
 @rt("/")
+
 async def get(request):
     new_frm = Form(hx_post='/', target_id='todo-list', hx_swap='beforeend')(
         Group(
